093d2b1c56834e1dd4ca1cde7992abcd8e8e2620
Eddie Harvey <edharve@gmail.com>
2016-12-02 14:45:09 +0000

    Add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201204138) do
+ActiveRecord::Schema.define(version: 20161202143128) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,9 +18,15 @@ ActiveRecord::Schema.define(version: 20161201204138) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "architect"
-    
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
