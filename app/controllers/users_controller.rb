f33eb4049fbfeec3bd7a6cdd3dbdd6321941db24
Eddie Harvey <edharve@gmail.com>
2016-12-11 21:36:31 +0000

    Added user authentication throghout site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-	
+
+	before_action :set_user, only: [:edit, :update, :show]
+	before_action :require_same_user, only: [:edit, :update]
+
 	def new
 		@user = User.new
 	end
@@ -15,9 +18,7 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
-		@user.ip_address = session[:ip_address]
-		@location = session[:location]
+		
 	end
 
 	def index
@@ -26,11 +27,10 @@ class UsersController < ApplicationController
 	
 
 	def edit
-		@user = User.find(params[:id])
+		
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 		flash[:notice] = "User was successfully updated"
 		redirect_to articles_path
@@ -41,11 +41,22 @@ class UsersController < ApplicationController
 
 
 	private
+
+	def set_user
+		@user = User.find(params[:id])
+		@user.ip_address = session[:ip_address]
+		@location = session[:location]
+	end
 	
 	def user_params
 	params.require(:user).permit(:username, :email, :password)
 	end
 
-
+	def require_same_user
+		if !logged_in? && current_user != @user
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
+	end
 
 end
