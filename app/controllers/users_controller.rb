c1839a0c79b1c76708c1c53d1b9f05579b4bb57a
Eddie Harvey <edharve@gmail.com>
2016-12-12 19:43:55 +0000

    Tidy up formatting, add auto-login and flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,18 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+		session[:user_id] = @user.id
+		session[:ip_address] = request.remote_ip
 	    flash[:notice] = "Welcome to the Glasgow Pevsner #{@user.username}"
-	    redirect_to articles_path
+	    redirect_to user_path(@user)
     	else
     		render 'new'
     	end
 	end
 
 	def show
-		
+		@user.ip_address = session[:ip_address]
+		@location = session[:location]
 	end
 
 	def index
